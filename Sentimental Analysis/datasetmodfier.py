2d1a249cd19b09e9165b2ec759184249341295a7
bobbywong234 <36208684+bobbywong234@users.noreply.github.com>
2019-06-28 16:23:51 +0800

    Update datasetmodfier.py

--- Sentimental Analysis/datasetmodfier.py
+++ Sentimental Analysis/datasetmodfier.py
@@ -10,11 +10,17 @@ df=df.replace(np.nan,'',regex=True)
 
 n=-1
 i=0
+dr=[]
 for index,row in df.iterrows():
  n+=1
  if row['Translated_Review']=='' and row['Sentiment'] !='':
   i=n
   print(row['Translated_Review'],row['Sentiment'],i)
+  dr.append(i)
+
+
+for x in range(0,len(dr)):
+ df=df.drop(df.index[dr[x]])
   
   # import this script by using its file name
   # use exec(open('file_name.py').read()) to execute in python
